8e590516123276dd28e0cba4fcb4d37904dc53ab
Ashtell <mhallisawesome@gmail.com>
2018-01-11 18:57:59 -0700

    Added Multi-dice roll

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-function getRandomNumber(diceSize){
+
+function getRandomNumber(diceSize) {
     return Math.floor(Math.random() * diceSize + 1);
 }
-function displayNumber(id, diceSize){
+function displayNumber(id, diceSize) {
+    var amountOfTimes = document.getElementById("number").value;
     var theDisplay = document.getElementById(id);
-    theDisplay.innerHTML = getRandomNumber(diceSize);
+    var arrayofDice = [];
+    for(var i = 0; i < amountOfTimes; i++){
+        
+        arrayofDice[i] = getRandomNumber(diceSize);
+    }
+
+    theDisplay.innerHTML = arrayofDice.join(", ");
 }
-function getPercent(id){
-    var theDisplay = document.getElementById(id);
-    var number = getRandomNumber(100);
+function getPercent(id) {
+    var theDisplay = document.getElementById(id),
+        number = getRandomNumber(100);
     theDisplay.innerHTML = number + "%";
 }
